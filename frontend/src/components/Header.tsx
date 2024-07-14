b65227a1833af6235ec02009266b7351b9a10006
mohammed shibili cv <mohshibilicv@gmail.com>
2024-07-14 15:40:41 +0530

    style: header data table button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <p className="text-black dark:text-white font-bold text-xl">LOGO</p>
       </Link>
       <div className="flex items-center justify-center gap-3">
-        <Link to="/table" className="px-2 py-2 text-xs sm:text-sm font-semibold rounded border border-gray-500 dark:border-gray-500 hover:border-dashed">View Data Table</Link>
+        <Link to="/table" className="px-2 py-2.5 text-xs sm:text-sm font-semibold rounded border border-gray-200 hover:border-gray-500 dark:border-gray-800 hover:dark:border-gray-500 hover:border-dashed">View Data Table</Link>
         <ModeToggle />
       </div>
     </div>
